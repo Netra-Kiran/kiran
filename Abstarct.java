069c31c90294ba8e6802afa9af533dcde3b69a9e
Netra-Kiran <123362942+Netra-Kiran@users.noreply.github.com>
2023-02-24 12:01:54 +0530

    Add files via upload

---
+++ Abstarct.java
@@ -0,0 +1,44 @@
+interface User
+{
+    public void login();
+    public void logout();
+}
+class Webuser implements User
+{
+  @Override
+  public void login()
+  {
+    System.out.println("the webuser is login using pwd:");
+
+  }
+ @Override
+  public void logout()
+  {
+    System.out.println("the webuser is logging out");
+  }
+}
+class Admin implements User
+{
+    @Override
+    public void login()
+    {
+        System.out.println("the admin is login using pwd:");  
+    }
+    @Override
+    public void logout()
+    {
+        System.out.println("the admin is logging out");
+    }
+}
+class Abstract
+{
+    public static void main(String[] args) 
+    {
+      Webuser w1=new Webuser();
+      w1.login();
+      w1.logout();
+      Admin a1=new Admin();
+      a1.login();
+      a1.logout();  
+    }
+}
